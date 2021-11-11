cc1003b2e63974aeb762bb0c4859250ecc3287e4
Artem <lenovouser2010u@gmail.com>
2021-11-11 11:05:07 +0200

    Add FileAnalyzer and FileManager but not have test

---
+++ src/com/luxoft/io/fileanalyzer/FileAnalyzer.java
@@ -0,0 +1,41 @@
+package com.luxsoft.io.fileanalyzer;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+public class FileAnalyzer {
+    public static void main(String[] args) throws IOException {
+        searchAndCountTheWord("story.txt", "the");
+        searchSentenceWithTheWord("story.txt", "the");
+    }
+    static String readContent(String path) throws IOException {
+        File pathToFile = new File(path);
+        InputStream inputStream = new FileInputStream(path);
+        int fileLength = (int) pathToFile.length();
+        byte[] contentArray = new byte[fileLength];
+        inputStream.read(contentArray);
+        return new String(contentArray);
+    }
+    public static void searchAndCountTheWord(String filePath, String wordSearch) throws IOException {
+        int count = 0;
+        String text = readContent(filePath);
+        String[] words = text.toLowerCase().replaceAll("[-.?!)(,:]", "").split("\\s");
+        for (String word : words) {
+            if (word.equals(wordSearch)) {
+                count++;
+            }
+        }
+        String result = "The word " + "'" + wordSearch + "'"+ " meets " + count + " times in this file";
+        System.out.println(result);
+    }
+    public static void searchSentenceWithTheWord(String filePath, String wordFind) throws IOException {
+        String text = readContent(filePath);
+        String[] sentences = text.toLowerCase().split("(?<=[.!?])\\s");
+        List<String> result = new ArrayList<>();
+        for (String sentence : sentences) {
+            if (sentence.contains(wordFind)) {
+                System.out.println(sentence);
+                result.add(sentence);
+            }
+        }
+    }
+}
---
+++ src/com/luxoft/io/filemanager/FileManager.java
@@ -0,0 +1,62 @@
+package com.luxsoft.io.filemanager;
+import java.io.*;
+import java.util.Objects;
+public class FileManager {
+    public static int countFiles(String path) {
+        File file = new File(path);
+        int count = 0;
+        for(File o : Objects.requireNonNull(file.listFiles())){
+            if(o.isDirectory()){
+                count = count + countFiles(o.getPath());
+            }
+            count++;
+        }
+        return count;
+    }
+    public static int countDirs(String path) {
+        File file = new File(path);
+        int countDir = 0;
+        for(File o : Objects.requireNonNull(file.listFiles())){
+            if(o.isDirectory()){
+                countDir = countDir + countDirs(o.getAbsolutePath());
+            }
+            countDir++;
+        }
+        return countDir;
+    }
+    public static void copy(String from, String to) throws IOException {
+        File file = new File(from);
+        File file1 = new File(to);
+        for(File o : Objects.requireNonNull(file.listFiles())){
+            if(o.isDirectory()){
+                copy(o.getAbsolutePath(), file1.getAbsolutePath() + "\\" + o.getName());
+            }
+            File newFile = new File(file1.getAbsolutePath()  + "\\" + o.getName());
+            newFile.createNewFile();
+            copyFileContent(o, newFile);
+        }
+    }
+    public static void copyFileContent(File from, File to) throws IOException {
+        OutputStream output = new FileOutputStream(to);
+        InputStream input  = new FileInputStream(from);
+        int current;
+        while((current = input.read()) != -1) {
+            output.write(current);
+        }
+        output.close();
+        input.close();
+    }
+    public static void move(String from, String to) throws IOException {
+        copy(from, to);
+        File src = new File(from);
+        delete(src);
+    }
+    public static void delete(File element) {
+        if (element.isDirectory()) {
+            for (File sub : Objects.requireNonNull(element.listFiles())) {
+                delete(sub);
+            }
+        }
+        element.delete();
+    }
+}
